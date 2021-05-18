f57c68127acc4b1aaefdd82012a732b82876f268
Pedro H. Silva <pedroh_silva@outlook.com>
2021-05-18 08:29:12 -0300

    A classe SocketBuilder foi criada para manipulara as conexoes

---
+++ app/pages/_shared/constants.js
@@ -0,0 +1,13 @@
+export const constants = {
+    socketUrl: 'http://localhost',
+    socketNamespaces: {
+        room: 'room',
+        lobby: 'lobby'
+    },
+    events: {
+        USER_CONNECTED: 'userConnected',
+        USER_DISCONNECTED: 'userDisconnect',
+
+        JOIN_ROOM: 'joinRoom'
+    }
+}
---
+++ app/pages/room/src/index.js
@@ -0,0 +1,24 @@
+import SocketBuilder from "../../_shared/socketBuilder.js"
+import { constants } from "../../_shared/constants.js"
+
+const socketBuilder = new SocketBuilder({
+    socketUrl: constants.socketUrl,
+    namespace: constants.socketNamespaces.room
+})
+
+const socket = socketBuilder
+    .setOnUserConnected(() => console.log('user connected!', user))
+    .setOnUserDisconnected(() => console.log('user disconnectd!', user))
+    .build()
+
+const room = {
+    id: Date.now(),
+    topic: 'JS Expert éh noix'
+}
+
+const user = {
+    img: 'https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-256.png',
+    username: 'tuxpedro'
+}
+
+socket.emit(constants.events.JOIN_ROOM, { user, room })
